a7f40cfa5181153719d349898f6750cf873c1dee
emilykamiti <emilykamiti@gmail.com>
2023-02-02 11:30:53 -0800

    mit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-""" Function that deploys """
-from datetime import datetime
-import os
-import shlex
+""" Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy """
 
 
 from fabric.api import put, run, env, sudo
@@ -25,13 +22,12 @@ def deploy():
 
 def do_pack():
     try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except:
         return None
 
@@ -41,3 +37,17 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except error as e:
+        return False
